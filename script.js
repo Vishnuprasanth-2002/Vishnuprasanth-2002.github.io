4c1a12ab3938454399e67ea2150f451427e56d5e
Vishnuprasanth-2002 <s.vishnuprasanth14@gmail.com>
2024-02-04 02:55:00 +0530

    skills added

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   navlist.classList.remove("active");
 };
+
+// skills
+const skillsContent = document.getElementsByClassName("skills_content");
+
+skillsHeader = document.querySelectorAll(".skills_header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass == "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
+
+skillsHeader.forEach((element) => {
+  element.addEventListener("click", toggleSkills);
+});
